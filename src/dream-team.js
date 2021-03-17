1ae0d481a5a7519476163ecbb43fdb23dbfea8b5
NatalliaKri <55781208+NatalliaKri@users.noreply.github.com>
2021-03-17 21:20:47 +0300

    init: implement function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
+
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  
+    if(!(members instanceof Array)) return false;
+    let arr = [];
+
+    for(let i = 0; i < members.length; i++)
+    { 
+        if(typeof members[i] !== 'string') arr.push('');
+		
+		
+		if(typeof members[i] === 'string'){
+		
+        let memberTrim = members[i].trim();
+        arr.push(memberTrim[0].toUpperCase());
+		} 
+    }
+    return arr.sort().join('');
 };
