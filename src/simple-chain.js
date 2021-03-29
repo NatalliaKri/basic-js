4aafc911f19027fb14229f147034793cc6fd5f72
NatalliaKri <55781208+NatalliaKri@users.noreply.github.com>
2021-03-29 10:23:39 +0300

    init: implement function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,48 @@
+
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+	
+  currentChain: [],
+	
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.currentChain.length;
   },
+  
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+	  this.currentChain.push('( ' + value + ' )');
+	  return this;
+	  },
+	   
+  removeLink(position) {  
+	  if(typeof position === 'number' && Number.isInteger(position)){
+      
+		  if(position < 1|| position> this.getLength()){
+			  this.currentChain.length = 0;
+			  throw new Error(' Position is not correct or nonexistant!');  
+		  };
+		  
+		  let index = position - 1;
+		  this.currentChain.splice(index, 1);
+		  return this;
+	  };
+	  
+	  this.currentChain.length = 0;
+	  throw new Error(' Position is not integer number! ');  
   },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
+   
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.currentChain.reverse();
+	  return this;
   },
+  
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+	  let chain = this.currentChain.join('~~');
+	  this.currentChain.length = 0;
+	  return chain;
+  },  
 };
 
 module.exports = chainMaker;
+
+	  
